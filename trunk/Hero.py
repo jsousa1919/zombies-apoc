d87f88c2572d2113cfad8993f32da255bcceb408
jsousa1919 <jsousa1919@329dcae6-5bb1-6761-3754-1261ca98e6f0>
2011-01-14 19:40:09 +0000

    Refactored media with media.py, updated zombie behavior

--- trunk/Hero.py
+++ trunk/Hero.py
@@ -1,8 +1,6 @@
-import pygame, sys, os, Zombie, random, math
+import pygame, sys, os, Zombie, random, math, media
 from pygame.locals import *
 
-WINDOW_HEIGHT = 800
-WINDOW_WIDTH = 1000
 RUNSPEED = 24
 WALKSPEED = 15
 LEFT = 0
@@ -19,69 +17,25 @@ class Hero(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
 		self.screen = pygame.display.get_surface()
-
-		self.master_image = pygame.image.load(os.path.join("data","male_unarmored.png")).convert_alpha()
-
-		self.idle_sprites = self.__load_idle_sprites__()
-		self.walk_sprites = self.__load_walk_sprites__()
-		self.run_sprites = self.__load_run_sprites__()
 			  
 		self.direction = 0
 		self.MOVE = False
 		self.RUN = False
 		self.speed = WALKSPEED
 
-		self.rect = self.idle_sprites[0][0].get_rect()
 		self.frame = 0
 
 		self.area = self.screen.get_rect()
 
 		random.seed()
-		x = random.random() * WINDOW_WIDTH
-		y = random.random() * WINDOW_HEIGHT
+		x = random.random() * media.WINDOW_WIDTH
+		y = random.random() * media.WINDOW_HEIGHT
 
-		self.rect.topleft = x, y
+		self.rect = media.HERO_SPRITE_IDLE[0][0].get_rect(center=(x, y))
+		self.image = media.HERO_SPRITE_IDLE[0][0]
 
 		self.update()
-    
-	def __load_idle_sprites__(self, w = 256, h = 256):
-		'''
-		THE HERO SPRITE HAS A WIDTH, HEIGHT of 256
-		'''
-		images = []
 
-		for i in xrange(0,8):
-		    images.append([])
-		    for j in xrange(0,1):
-			images[i].append(self.master_image.subsurface((j*w, i*h, w, h)))
-
-		return images
-
-	def __load_walk_sprites__(self, w = 256, h = 256):
-		'''
-		THE HERO SPRITE HAS A WIDTH, HEIGHT of 256
-		'''
-		images = []
-
-		for i in xrange(0,8):
-		    images.append([])
-		    for j in xrange(0,4):
-			images[i].append(self.master_image.subsurface((j*w, i*h, w, h)))
-
-		return images
-
-	def __load_run_sprites__(self, w = 256, h = 256):
-		'''
-		THE HERO SPRITE HAS A WIDTH, HEIGHT of 256
-		'''
-		images = []
-
-		for i in xrange(0,8):
-		    images.append([])
-		    for j in xrange(4,6):
-			images[i].append(self.master_image.subsurface((j*w, i*h, w, h)))
-
-		return images
 
 	def update(self):
 		self.frame += 1
@@ -101,11 +55,11 @@ class Hero(pygame.sprite.Sprite):
 				if randsound:
 				    randsound.play()
 		
-				if self.frame >= len(self.run_sprites[self.direction]): self.frame = 0
-				self.image = self.walk_sprites[self.direction][self.frame]
+				if self.frame >= len(media.HERO_SPRITE_RUN[self.direction]): self.frame = 0
+				self.image = media.HERO_SPRITE_RUN[self.direction][self.frame]
 			else:
-				if self.frame >= len(self.walk_sprites[self.direction]): self.frame = 0
-				self.image = self.walk_sprites[self.direction][self.frame]
+				if self.frame >= len(media.HERO_SPRITE_WALK[self.direction]): self.frame = 0
+				self.image = media.HERO_SPRITE_WALK[self.direction][self.frame]
 
 			sqrt_2 = math.sqrt(2)
 			if self.direction == 0:
@@ -131,8 +85,8 @@ class Hero(pygame.sprite.Sprite):
 			    
 			self.MOVE = False
 		else:
-			if self.frame >= len(self.idle_sprites[self.direction]): self.frame = 0
-			self.image = self.idle_sprites[self.direction][self.frame]
+			if self.frame >= len(media.HERO_SPRITE_IDLE[self.direction]): self.frame = 0
+			self.image = media.HERO_SPRITE_IDLE[self.direction][self.frame]
 
 	def move(self, dirindex):
 		self.direction = dirindex
--- trunk/Zombie.py
+++ trunk/Zombie.py
@@ -1,8 +1,5 @@
-import pygame, sys, os, random, timer, math, time, operator
+import pygame, sys, os, random, timer, math, time, operator, media
 from pygame.locals import *
-
-WINDOW_HEIGHT = 800
-WINDOW_WIDTH = 1000
 
 MAXSPEED = 12
 MIDSPEED = 8
@@ -28,17 +25,10 @@ DOWN = 7
 DOWNLEFT = 0
 
 class Zombie(pygame.sprite.Sprite):
+#init
 	def __init__(self, herogroup):
 		pygame.sprite.Sprite.__init__(self) #call Sprite intializer
-		pygame.mixer.init(11025)
 		self.screen = pygame.display.get_surface()
-		self.fov_image = pygame.Surface((400,400))
-		pygame.draw.polygon(self.fov_image, pygame.Color('0xFF000040'), [(200, 200), (400, 100), (400, 300)])
-
-		self.master_image = pygame.image.load(os.path.join("data","zombie_1.png")).convert_alpha()
-
-		self.idle_sprites = self.__load_idle_sprites__()
-		self.walk_sprites = self.__load_walk_sprites__()
 
 		self.heroes = herogroup
 		self.hero_focus = None
@@ -55,49 +45,97 @@ class Zombie(pygame.sprite.Sprite):
 		self.speaking = False
 		self.MOVE = False
 
-		self.rect = self.idle_sprites[0][0].get_rect()
-
 		self.frame = 0
 
 		self.area = self.screen.get_rect()
 		self.speed = MINSPEED
 
 		random.seed()
-		x = random.random() * WINDOW_WIDTH
-		y = random.random() * WINDOW_HEIGHT
+		x = random.random() * media.WINDOW_WIDTH
+		y = random.random() * media.WINDOW_HEIGHT
 
-		self.rect.topleft = x, y
+		self.image = media.ZOM_SPRITE_IDLE[0][0]
+		self.rect = media.ZOM_SPRITE_IDLE[0][0].get_rect(center=(x,y))
+
 		self.lkl = x, y
 
 		self.update()
 
+#update
+	def update(self):
+		
+		self.tick += 1
 
-	def __load_idle_sprites__(self, w = 128, h = 128):
-		'''
-		ZOMBIE SPRITES HAVE A WIDTH, HEIGHT of 128
-		'''
-		images = []
+		#scale frame switching to zombie speed
+		if self.tick % int(MAXSPEED / self.speed) == 0: 
+			self.frame += 1			
 
-		for i in xrange(0, 8):
-			images.append([])
-			for j in xrange(0, 4):
-				images[i].append(self.master_image.subsurface((j*w, i*h, w, h)))
+		#create field of view mask
+		fov_image = pygame.transform.rotate(media.ZOM_FOV, self.angle)
+		fov_image.set_colorkey(pygame.Color('black'))
+		fov_rect = fov_image.get_rect(center=self.rect.center)
+		fov_mask = pygame.mask.from_surface(fov_image)
 
-		return images
+		# for viewing FOV
+		#fov_image = self.image = pygame.transform.rotate(media.ZOM_FOV, self.angle)
+		#self.image.set_colorkey(pygame.Color('black'))
+		#self.rect = self.image.get_rect(center=self.rect.center)
+		#fov_mask = pygame.mask.from_surface(self.image)
 
-	def __load_walk_sprites__(self, w = 128, h = 128):
-		'''
-		ZOMBIE SPRITES HAVE A WIDTH, HEIGHT of 128
-		'''
-		images = []
+		#interact with heroes
+		for hero in self.heroes:
+			hero_center = hero.rect.center
+			world_diff = map(operator.sub, hero_center, self.rect.center) # actual position difference
+			local_diff = map(operator.sub, fov_image.get_rect().center, hero.image.get_rect().center) # difference to account for sprite size
+			diff = map(operator.add, world_diff, local_diff)
 
-		for i in xrange(0,8):
-			images.append([])
-			for j in xrange(4,12):
-				images[i].append(self.master_image.subsurface((j*w, i*h, w, h)))
+			if fov_mask.overlap(media.HERO_MASK, diff):
+				#start hero sighted
+				#self.speak() 
+				#print "attack!"
+				self.hero_focus = hero
+				self.lkl = hero_center
+				self.lt = self.tick
+				self.state = ST_ATTACKING
+				self.speed = MIDSPEED
 
-		return images
+		#state specific actions
+		if self.state == ST_ATTACKING and self.lt != self.tick:
+			self.state = ST_SEARCHING
+	
+		if self.state > ST_IDLE and (self.tick - self.lt) > (self.endurance * 100):
+			self.stop()
+			self.speed = MINSPEED
+			self.state = ST_IDLE
 
+		if self.state < ST_ATTACKING:
+			if (self.tick - self.lt) % (self.focus * 25) == 0:
+				#print "change"
+				self.rand_lkl(50)
+			#if self.MOVE == False:
+			#	self.add_to_angle(self.focus * (random.random() - 0.5) * 20)
+
+		self.goto_lkl()
+
+		#draw zombie
+		if self.MOVE:
+			if self.frame >= len(media.ZOM_SPRITE_WALK[self.direction]): self.frame = 0
+			im = media.ZOM_SPRITE_WALK[self.direction][self.frame]
+			#self.image.blit(im, map(operator.sub, self.image.get_rect().center, im.get_rect().center)) # for viewing FOV
+			self.image = im																				# for invisible FOV
+
+			x = self.speed*math.cos(math.radians(self.angle))
+			y = self.speed*math.sin(math.radians(self.angle))
+			self.rect.centerx += x
+			self.rect.centery -= y
+
+		else:
+			if self.frame >= len(media.ZOM_SPRITE_IDLE[self.direction]): self.frame = 0
+			im = media.ZOM_SPRITE_IDLE[self.direction][self.frame]
+			#self.image.blit(im, map(operator.sub, self.image.get_rect().center, im.get_rect().center))	# for viewing FOV
+			self.image = im																				# for invisible FOV
+
+#actions
 	def speak(self):
 		if self.speaking:
 			return
@@ -111,6 +149,7 @@ class Zombie(pygame.sprite.Sprite):
 		t = timer.ttimer(0.5, 1, self.endspeak, channel)
 		t.Start()
 
+
 	def endspeak(self, channel):
 		if not channel.get_busy():
 			self.speaking = False
@@ -118,74 +157,20 @@ class Zombie(pygame.sprite.Sprite):
 		else:
 			t = timer.ttimer(0.5, 1, self.endspeak, channel)
 			t.Start()
-	
-	def update(self):
-		self.tick += 1
 		
-		if self.tick % int(MAXSPEED / self.speed) == 0: 
-			self.frame += 1
 
-		#self.goto_lkl()
-		self.image = pygame.transform.rotate(self.fov_image, self.angle)
-		self.rect = self.image.get_rect(center=self.rect.center)
-		self.image.set_colorkey(pygame.Color('black'))
-
-		fov = pygame.mask.from_surface(self.image)
-		for hero in self.heroes:
-			hero_center = hero.rect.center
-			hero_mask = pygame.mask.from_surface(hero.image)
-			world_diff = map(operator.sub, hero.rect.center, self.rect.center)
-			local_diff = map(operator.sub, self.image.get_rect().center, hero.image.get_rect().center)
-			diff = map(operator.add, world_diff, local_diff)
-			if fov.overlap(hero_mask, diff):
-				#start hero sighted
-				#self.speak() 
-				self.hero_focus = hero
-				self.lkl = hero_center
-				self.lt = self.tick
-				self.state = ST_ATTACKING
-				self.speed = MIDSPEED
-
-		if self.state > ST_IDLE and self.tick - self.lt > self.endurance * 100:
-			self.stop()
-			self.speed = MINSPEED
-			self.state = ST_IDLE
-
-		if self.state == ST_IDLE:
-			if self.tick % (self.focus * 1000) == 0:
-				print "change"
-				self.lkl = self.lkl[0] + ((random.random() - 0.5) * 3000), self.lkl[1] + ((random.random() - 0.5) * 3000)
-			self.goto_lkl(MAXSPEED,50)
-			if self.MOVE == False:
-				self.add_to_angle(self.focus * (random.random() - 0.5) * 20)
-
-		if self.state > ST_IDLE:
-			self.goto_lkl()
-
-		if self.MOVE:
-			if self.frame >= len(self.walk_sprites[self.direction]): self.frame = 0
-			im = self.walk_sprites[self.direction][self.frame]
-			#self.image.blit(im, map(operator.sub, self.image.get_rect().center, im.get_rect().center))
-			self.image = im
-			self.rect = im.get_rect(center=self.rect.center)
-
-			x = self.speed*math.cos(math.radians(self.angle))
-			y = self.speed*math.sin(math.radians(self.angle))
-			self.rect.centerx += x
-			self.rect.centery -= y
-
-		else:
-			if self.frame >= len(self.idle_sprites[self.direction]): self.frame = 0
-			im = self.idle_sprites[self.direction][self.frame]
-			#self.image.blit(im, map(operator.sub, self.image.get_rect().center, im.get_rect().center))
-			self.image = im
-			self.rect = im.get_rect(center=self.rect.center)
-		
 	def face(self, dirindex):
 		if self.direction != dirindex: 
 			#self.speak()
 			self.direction = dirindex
 		
+#movement
+	def move(self):
+		self.MOVE = True
+	def stop(self):
+		self.MOVE = False
+
+
 	def set_angle(self, theta):
 		ang = int(theta % 360)
 		self.angle = ang
@@ -212,23 +197,20 @@ class Zombie(pygame.sprite.Sprite):
 	def add_to_angle(self, delta):
 		self.set_angle(self.angle + delta)
 
-	def move(self):
-		self.MOVE = True
-	def stop(self):
-		self.MOVE = False
+	def rand_lkl(self, rmin = 0, rmax = 200):
+		x = random.randrange(rmin, rmax, 1)
+		if x % 2 == 0: x = -x
+		y = random.randrange(rmin, rmax, 1)
+		if y % 2 == 0: y = -y
+		self.lkl = self.lkl[0] + x, self.lkl[1] + y
 
-	def goto_lkl(self, nogo = 65, rr = 20):
+	def goto_lkl(self, nogo = 65):
 		x, y = self.lkl
-		x += ((random.random() - 0.5) * rr)
-		y += ((random.random() - 0.5) * rr)
 		dist = math.hypot(y - self.rect.centery, x - self.rect.centerx)
-		self.lkl = x, y
-
+		ang = math.degrees(math.atan2((self.rect.centery - y),(x - self.rect.centerx)))
+		self.set_angle(ang)
+		
 		if dist <= nogo:
 			self.stop()
 		else:
 			self.move()
-			ang = math.degrees(math.atan2((self.rect.centery - y),(x - self.rect.centerx)))
-			self.set_angle(ang)
-		
-		
