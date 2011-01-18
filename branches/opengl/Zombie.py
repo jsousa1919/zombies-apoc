b33ab7c37c384ddc608ead98939fe9010f1b2ef3
jsousa1919 <jsousa1919@329dcae6-5bb1-6761-3754-1261ca98e6f0>
2011-01-18 04:06:06 +0000

    refactored zombie class

--- branches/opengl/Zombie.py
+++ branches/opengl/Zombie.py
@@ -12,12 +12,14 @@ MINENDUR = 1
 MAXENDUR = 10
 MINSIGHT = 1
 MAXSIGHT = 10
+MAXHEALTH = 10
 
+ST_DEAD = -1
 ST_IDLE = 0
 ST_SEARCHING = 1
-#ST_SWARMING
-#ST_CHASING
-ST_ATTACKING = 2
+ST_SWARMING = 2
+ST_CHASING = 3
+ST_ATTACKING = 4
 
 class Zombie():
 #init
@@ -27,22 +29,31 @@ class Zombie():
 
 		self.focus = 4
 		self.endurance = 4
-		self.sight = 3
+		self.sight = 2
+		self.hp = 10
 
 		Util.request_fov(self.sight)
 
+		self.last_search = 0
+		self.last_seen = 0
+		self.last_swarm = 0
+		self.start_swarm_tick = 0
+		self.end_slow = 0
+
 		self.hero_focus = None
 		self.state = ST_IDLE
-		self.lt = 0
 		self.swarming = False
 		self.speaking = False
-		self.MOVE = False
+		self.is_slow = False
+		self.moving = False
 		self.frame = 0
-		self.angle = random.randrange(0,360)
 		self.tick = 0
+		self.angle = random.randrange(0,360)
+		self.angle_target = random.randrange(0,360)
 		
 		self.speed_mod = random.randrange(0, MINSPEED)
-		self.set_speed(MINSPEED)
+		self.speed = MINSPEED
+		self.speed_target = MINSPEED
 
 		random.seed()
 		self.posx = random.random() * Util.WINDOW_WIDTH
@@ -51,14 +62,18 @@ class Zombie():
 		self.rect.center = (self.posx, self.posy)
 
 		self.lkl = self.rect.center
+		self.dist = 0
 
 		self.update()
 		
 	def getDisplayList(self):
-		if self.MOVE:
+		if self.state == ST_DEAD:
+			return Util.ZOM_DEAD_DISPLIST[self.frame % len(Util.ZOM_SPRITE_DEAD)]
+		elif self.moving:
 			return Util.ZOM_WALK_DISPLIST[self.frame % len(Util.ZOM_SPRITE_WALK)]
 		else:
 			return Util.ZOM_IDLE_DISPLIST[self.frame % len(Util.ZOM_SPRITE_IDLE)]
+
 			
 	def draw(self):
 		glLoadIdentity()
@@ -73,12 +88,50 @@ class Zombie():
 
 #update
 	def update(self):
+		if self.state == ST_DEAD and self.frame == len(UTIL.ZOM_DEAD_DISPLIST)-1:
+			return
 		self.tick += 1
 
-		#scale frame switching to zombie speed
-		if self.tick % int(MAXSPEED + self.speed_mod / self.speed) == 0: 
-			self.frame += 1
+		#for hero in self.heroes:
+		self.test_fov(self.hero)
 
+		#state specific actions	
+	
+		if self.state == ST_IDLE:
+			self.set_speed(MINSPEED)
+			if (self.tick - self.last_search) > random.randrange(10 * self.focus * self.endurance, 15 * MAXFOCUS * MAXENDUR):
+				self.investigate()				
+			
+		if self.state == ST_SEARCHING:
+			if (self.tick - self.last_search) > random.randrange(10 * self.focus * self.endurance, 15 * MAXFOCUS * MAXENDUR):
+				self.investigate()	
+			if (self.tick - self.last_seen) > random.randrange(20 * self.endurance, 20 * MAXENDUR):
+				self.state = ST_IDLE
+
+		if self.state == ST_SWARMING:
+			if (self.tick - self.start_swarm_tick) > random.randrange(25 * self.endurance, 25 * MAXENDUR):
+				self.slow(4)
+
+		if self.state == ST_CHASING:
+			self.set_speed(MIDSPEED)
+			if (self.last_seen - self.tick) > 5:
+				self.state = ST_SEARCHING
+			self.swarm.add(self.rect.center, self.lkl)
+			if self.dist < 10:
+				self.state = ST_ATTACKING
+
+		if self.state == ST_ATTACKING:
+			if self.dist > 10:
+				self.state = ST_CHASING
+
+		#check ailments
+		if self.is_slow and self.tick > self.end_slow:
+			self.is_slow == False
+
+		self.goto_lkl()
+		self.move()
+	
+	def test_fov(self, hero):
 		#create field of view mask
 		ang = int(self.angle / Util.ZOM_FOV_DEV)
 		fov_image = Util.ZOM_FOV[self.sight][ang]
@@ -97,41 +150,10 @@ class Zombie():
 			#print "attack! ", hero_center
 			self.hero_focus = self.hero
 			self.lkl = hero_center
-			self.lt = self.tick
-			self.state = ST_ATTACKING
-			self.end_swarm()
+			self.last_seen = self.tick
+			self.state = ST_CHASING
 			self.set_speed(MIDSPEED)
 
-		#state specific actions	
-	
-		if self.state == ST_ATTACKING:
-			self.swarm.add(self.rect.center, self.lkl)
-
-		if self.state == ST_ATTACKING and self.tick - self.lt > 10:
-			self.state = ST_SEARCHING
-	
-		if self.state > ST_IDLE and (self.tick - self.lt) > (self.endurance * 25):
-			self.stop()
-			self.set_speed(MINSPEED)
-			self.state = ST_IDLE
-
-		if self.state < ST_ATTACKING:
-			if (self.tick - self.lt) == random.randrange(75, (self.focus * self.speed * 25)):
-				self.rand_lkl(100)
-
-		self.goto_lkl()
-		
-		if self.MOVE:
-			if self.frame >= len(Util.ZOM_SPRITE_WALK): self.frame = 0
-
-			x = self.speed*math.cos(math.radians(self.angle))
-			y = self.speed*math.sin(math.radians(self.angle))
-			self.center(self.posx + x, self.posy + y)
-
-		else:
-			self.end_swarm()
-			if self.frame >= len(Util.ZOM_SPRITE_IDLE): self.frame = 0
-	
 #actions
 	def speak(self):
 		if self.speaking:
@@ -156,73 +178,124 @@ class Zombie():
 			t.Start()
 		
 #movement
-	def move(self):
-		self.MOVE = True
+	def go(self):
+		self.moving = True
+
 	def stop(self):
 		self.swarming = False
-		self.MOVE = False
+		self.moving = False
 
-	def center(self, x, y = None):
+	def set_center(self, x, y = None):
 		if y != None:
-			(self.posx, self.posy) = self.rect.center = (x, y)
+			self.rect.center = (self.posx, self.posy) = (x, y)
 		else:
-			(self.posx, self.posy) = self.rect.center = x	
+			self.rect.center = (self.posx, self.posy) = x	
+
+	def add_center(self, x, y = None):
+		if y != None:
+			self.rect.center = (self.posx, self.posy) = (self.posx + x, self.posy + y)
+		else:
+			self.rect.center = (self.posx, self.posy) = (self.posx + x[0], self.posy + x[1])
 
 	def set_speed(self, speed):
-		self.speed = speed + self.speed_mod
+		speed += self.speed_mod
+		if self.is_slow:
+			speed = speed / 2
+		self.speed_target = speed
 
 	def set_angle(self, theta):
-		ang = int(theta % 360)
-		self.angle = ang
+		ang = int(theta) % 360
+		self.angle_target = ang
 
-	def rotate(self, delta):
-		self.set_angle(self.angle + delta)
+	def move(self):	
+		if self.tick % int(MAXSPEED + self.speed_mod / self.speed) == 0: 
+			self.frame += 1
+		if self.moving:
+			if self.frame >= len(Util.ZOM_SPRITE_WALK): self.frame = 0
 
-	def rand_lkl(self, rmin = 0, rmax = 200):
+			if self.speed < self.speed_target: self.speed += 1
+			elif self.speed > self.speed_target: self.speed -= 1
+
+			ldiff = (self.angle_target - self.angle) % 360
+			rdiff = (self.angle - self.angle_target) % 360
+			if (ldiff < rdiff):
+				dt = (self.angle + (ldiff / 5)) % 360
+			else:
+				dt = (self.angle - (rdiff / 5)) % 360
+			self.angle = dt
+
+			
+			
+			#print self.speed
+			dx = self.speed*math.cos(math.radians(self.angle))
+			dy = self.speed*math.sin(math.radians(self.angle))
+			#print self.rect.center, " to ", self.lkl, " at ", self.angle
+			self.add_center(dx, dy)
+
+		else:
+			self.end_swarm()
+			if self.frame >= len(Util.ZOM_SPRITE_IDLE): self.frame = 0
+		
+
+#
+	def investigate(self, rmin = 50, rmax = 200):
 		x = random.randrange(rmin, rmax, 1)
 		if x % 2 == 0: x = -x
 		y = random.randrange(rmin, rmax, 1)
 		if y % 2 == 0: y = -y
-		self.lkl = self.lkl[0] + x, self.lkl[1] + y
-		if random.randrange(0, 100) < self.speed:
-			self.swarming = True
 
-	def goto_lkl(self, nogo = 20):
+		self.lkl = self.lkl[0] + x, self.lkl[1] + y
+		self.last_search = self.tick
+		#print self.rect.center, " investigate ", self.lkl
+
+		if random.randrange(0, 100) < self.speed:
+			self.last_seen = self.tick
+			self.try_swarm()
+
+	def goto_lkl(self, nogo = 60):
 		x, y = self.lkl
-		dist = math.hypot(y - self.posy, x - self.posx)
-		ang = math.degrees(math.atan2((y - self.posy),(x - self.posx)))
-		ldiff = (ang - self.angle) % 360
-		rdiff = (self.angle - ang) % 360
-		if (ldiff < rdiff):
-			self.rotate(ldiff / 5)
-		else:
-			self.rotate(-rdiff / 5)
+		self.dist = math.hypot(y - self.posy, x - self.posx)
+		ang = math.degrees(math.atan2(y - self.posy, x - self.posx))
+		self.set_angle(ang)
 		
-		if dist <= nogo:
+		if self.dist <= nogo:
 			self.stop()
 		else:
-			self.move()
-			x += random.randrange(0, self.focus * 4) - (self.focus * 2)
-			y += random.randrange(0, self.focus * 4) - (self.focus * 2)
-			self.lkl = (x, y)
+			self.go()
 
 	def try_swarm(self):
-		if self.state != ST_ATTACKING and not (self.swarming and self.tick % 5 > 0):
-			num, to = self.swarm.swarm_movement(self)
-			if num != 0 and random.randrange(0, 5 * self.focus) < num:
-				#print "swarm to", loc
-				self.lkl = to
-				self.lt = self.tick
-				self.state = ST_SEARCHING
-				self.swarming = True
+		if self.state == ST_ATTACKING or (self.tick - self.last_swarm) < (20 * self.focus) or (self.state == ST_SWARMING and (self.tick % 5) > 0):
+			return
+		num, to = self.swarm.swarm_movement(self)
+		if num != 0 and random.randrange(0, 5 * self.focus) < num:
+			self.start_swarm(to)
+
+	def start_swarm(self, to):
+		#print "swarm to", loc
+		self.lkl = to
+		self.last_seen = self.tick
+		if self.state != ST_SWARMING:
+			self.state = ST_SWARMING
+			self.start_swarm_tick = self.tick
+			
+
+	def end_swarm(self):
+		if self.state == ST_SWARMING:
+			self.investigate(50, 100)
+			self.state = ST_SEARCHING
+			self.last_swarm = self.tick
 
 	def prop_swarm(self):
-		if Util.SUPERSWARM and self.swarming:
+		if Util.SUPERSWARM and self.state == ST_SWARMING:
 			self.swarm.add(self.rect.center, self.rect.center)
-	
-	def end_swarm(self):
-		self.swarming == False
 
+	def slow(self, mod):
+		if self.is_slow:
+			self.end_slow += (mod * (MAXENDUR - self.endurance) * 4)
+		else:
+			self.end_slow = self.tick + (mod * (MAXENDUR - self.endurance) * 4)
+
+#collision
 	def collide(self, other):
 		x, y = other.rect.center
 		dist = math.hypot(y - self.posy, x - self.posx)
@@ -234,11 +307,16 @@ class Zombie():
 
 	def push(self, mag, ang):
 		ang %= 360
-		if abs((180 - (ang - self.angle)) > 150):
+		if abs(180 - (ang - self.angle)) > 150:
 			self.stop()
 		else:
-			self.move()
+			self.go()
 		x = mag*math.cos(math.radians(ang))
 		y = mag*math.sin(math.radians(ang))
-		self.center(self.posx + x, self.posy + y)
+		self.add_center(x, y)
+
+	def damage(self):
+		self.health -= 5
+		if self.health <= 0:
+			self.state = ST_DEAD
 			
